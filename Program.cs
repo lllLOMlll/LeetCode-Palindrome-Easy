dc707c635536b786dc87a4376454a474e04c2146
Louis-Olivier Major <louis.olivier.major@gmail.com>
2024-10-29 19:32:01 -0400

    Firt and last commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    public bool IsPalindrome(int x)
+    {     
+        string numberToString = x.ToString();
+        int numberLength = numberToString.Length;
+
+        for (int i = 0; i < numberLength/2; i++)
+        {
+            if (numberToString[i] != numberToString[numberLength- i - 1])
+            {
+                return false;
+            }
+        }
+
+        return true;  
+    }
+
+    static void Main(string[] args)
+    {
+        Program p = new Program();
+        Console.WriteLine("121: " + p.IsPalindrome(121));  
+        Console.WriteLine("4548: " + p.IsPalindrome(4548));
+        
+    }
+}
